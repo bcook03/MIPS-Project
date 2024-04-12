f1f4522302d782409980b55d6d95b6e45ff8e8f6
bcook03 <bnc121203@outlook.com>
2024-04-12 18:13:58 -0500

    removed extra square bracket

--- mipssim.cpp
+++ mipssim.cpp
@@ -136,7 +136,7 @@ int main(int argc, char* argv[] )
                 else if ( I.opcode ==  36) {
                     I.instStr = "BEQ\tR" + to_string(I.rt) + ", R" + to_string(I.rs) + ", #" + to_string(I.offset);
                     disout << I.binstr << " " << addr << "\t" << I.instStr << endl; 
-]                }
+                }
                 else if ( I.opcode == 32 && I.funct == 32) {
                     I.instStr = "ADD\tR" + to_string(I.rt) + ", R" + to_string(I.rs) + ", R" + to_string(I.rd);
                     disout << I.binstr << " " << addr << "\t" << I.instStr << endl; 
