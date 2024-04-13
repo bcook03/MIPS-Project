82769db7596e0c15f8c22a8bb8ac870283d54013
bcook03 <bnc121203@outlook.com>
2024-04-12 21:30:14 -0500

    decode format

--- mipssim.cpp
+++ mipssim.cpp
@@ -114,13 +114,13 @@ int main(int argc, char* argv[] )
                     I.instStr = to_string(i);
                     I.binStrSpace = I.binstr;
 
-                    disout << I.binstr << "\t" << I.instStr << endl;
-                    cout << I.binstr << "\t" << I.instStr << endl;
+                    disout << I.binstr << " " << addr << "\t" << I.instStr << endl;
+                    cout << I.binstr << " " << addr << "\t" << I.instStr << endl;
                 }
                 else if (I.valid == 0){
                 I.instStr = "Invalid Instruction";
-                disout << I.binstr << "\t" << I.instStr << endl;
-                cout << I.binstr << "\t" << I.instStr << endl;
+                disout << I.binstr << " " << addr << "\t" << I.instStr << endl;
+                cout << I.binstr << " " << addr << "\t" << I.instStr << endl;
                 }
            
                 else if (I.opcode == 34 ) {
